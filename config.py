ff4222ed2bb95894b30d96984069b2811e12bbc3
kamnii <kntskrbkv@gmail.com>
2022-10-31 23:19:40 +0600

    change 1

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from aiogram import Bot, Dispatcher
-from decouple import config
+# from decouple import config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
 storage = MemoryStorage()
-TOKEN = config("TOKEN")
+TOKEN = "5652445713:AAGeqGWfB8vrG6MByg90clrJivBFJrEPUAU"
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 ADMIN = [553984727, ]
