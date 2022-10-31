c074a16f02ab3f2e2f999002e6f7a372c499358b
kamnii <kntskrbkv@gmail.com>
2022-10-31 23:12:07 +0600

    admin_id from .env to config

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher
 from decouple import config
-from ast import literal_eval
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
@@ -8,5 +7,4 @@ storage = MemoryStorage()
 TOKEN = config("TOKEN")
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
-ADMIN = literal_eval(config("ADMIN"))
-# ADMIN = [553984727, ]
+ADMIN = [553984727, ]
